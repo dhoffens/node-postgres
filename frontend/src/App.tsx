70662c8d56f360a27bc7b2976bf1aa2835a42b82
Daniel Hoffens <dhoffens@gmail.com>
2024-03-19 18:12:15 -0400

    add login component, comment out merchant logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,86 +1,107 @@
 import { useState, useEffect, FC } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Login from './components/Login/Login';
+import Preferences from './components/Preferences/Preferences';
 import './App.css'
 
-type Merchant = {
-  id: number,
-  name: string,
-  email: string
-}
+// type Merchant = {
+//   id: number,
+//   name: string,
+//   email: string
+// }
 
 const App: FC = () => {
-  const [merchants, setMerchants] = useState<Merchant[]>([]);
+  const [token, setToken] = useState();
 
-  const getMerchant = () => {
-    fetch('http://localhost:3001')
-      .then((response) => response.json())
-      .then((data: Merchant[]) => {
-        setMerchants(data);
-      });
+  if(!token) {
+    return <Login setToken={setToken} />
   }
+  // const [merchants, setMerchants] = useState<Merchant[]>([]);
 
-  const createMerchant = () => {
-    let name = prompt('Enter merchant name');
-    let email = prompt('Enter merchant email');
-    fetch('http://localhost:3001/merchants', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name, email }),
-    })
-    .then((response) => {
-      if (!response.ok) {
-        console.error(response)
-      }
-      return response.json()
-    })
-    .then(data => {
-      alert(data);
-      getMerchant();
-    });
-  }
+  // const getMerchant = () => {
+  //   fetch('http://localhost:3001')
+  //     .then((response) => response.json())
+  //     .then((data: Merchant[]) => {
+  //       setMerchants(data);
+  //     });
+  // }
 
-  const deleteMerchant = () => {
-    let id = prompt('Enter merchant id');
-    if (id) {
-      fetch(`http://localhost:3001/merchants/${id}`, {
-        method: 'DELETE',
-      })
-      .then(response => response.json())
-      .then(data => {
-        alert(data);
-        getMerchant();
-      });
-    }
-  }
+  // const createMerchant = () => {
+  //   let name = prompt('Enter merchant name');
+  //   let email = prompt('Enter merchant email');
+  //   fetch('http://localhost:3001/merchants', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({ name, email }),
+  //   })
+  //   .then((response) => {
+  //     if (!response.ok) {
+  //       console.error(response)
+  //     }
+  //     return response.json()
+  //   })
+  //   .then(data => {
+  //     alert(data);
+  //     getMerchant();
+  //   });
+  // }
 
-  const updateMerchant = () => {
-    let id = prompt('Enter merchant id');
-    let name = prompt('Enter new merchant name');
-    let email = prompt('Enter new merchant email');
-    if (id) {
-      fetch(`http://localhost:3001/merchants/${id}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email }),
-      })
-        .then(response => response.text())
-        .then(data => {
-          alert(data);
-          getMerchant();
-        })
-    }
-  }
+  // const deleteMerchant = () => {
+  //   let id = prompt('Enter merchant id');
+  //   if (id) {
+  //     fetch(`http://localhost:3001/merchants/${id}`, {
+  //       method: 'DELETE',
+  //     })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       alert(data);
+  //       getMerchant();
+  //     });
+  //   }
+  // }
 
-  useEffect(() => {
-    getMerchant();
-  }, []);
+  // const updateMerchant = () => {
+  //   let id = prompt('Enter merchant id');
+  //   let name = prompt('Enter new merchant name');
+  //   let email = prompt('Enter new merchant email');
+  //   if (id) {
+  //     fetch(`http://localhost:3001/merchants/${id}`, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //       body: JSON.stringify({ name, email }),
+  //     })
+  //       .then(response => response.text())
+  //       .then(data => {
+  //         alert(data);
+  //         getMerchant();
+  //       })
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   getMerchant();
+  // }, []);
 
   return (
-    <div>
-      {merchants.length > 0 ? (
+    <div className='wrapper'>
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route 
+            path="/dashboard"
+            element={<Dashboard />}/>
+          <Route 
+            path="/preferences"
+            element={<Preferences />}/>
+        </Routes>
+      </BrowserRouter>
+
+      {/* {merchants.length > 0 ? (
         merchants.map((merchant) => (
           <div key={merchant.id}>
             {merchant.id} {merchant.name} {merchant.email}
@@ -92,7 +113,7 @@ const App: FC = () => {
       <br />
       <button onClick={deleteMerchant}>Delete merchant</button>
       <br />
-      <button onClick={updateMerchant}>Update merchant</button>
+      <button onClick={updateMerchant}>Update merchant</button> */}
     </div>
   )
 }
---
+++ frontend/src/components/Login/Login.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Login: React.FC = () => {
+    return (
+        <div className="login-wrapper">
+            <h1>Please log in</h1>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
