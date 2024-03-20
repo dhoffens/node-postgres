8ce97f3c18129bd6538afed36bd1e3e4a7e820ba
Daniel Hoffens <dhoffens@gmail.com>
2024-03-20 09:02:34 -0400

    login token

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,17 +1,46 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 import "./Login.css";
 
 interface LoginProps {
     setToken: (token: string) => void;
 }
 
+interface Credentials {
+    username: string;
+    password: string;
+}
+
 const Login: React.FC<LoginProps> = ({ setToken }) => {
     
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const [username, setUserName] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-        const token = 'sample-token';
+        console.log('submit', username, password);
+
+        const token = await loginUser({
+            username,
+            password
+        });
+
         setToken(token);
+    }
+
+    async function loginUser(credentials: Credentials): Promise<any> {
+        const response = await fetch('http://localhost:3001/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok!')
+        }
+
+        return response.json();
     }
     
     return (
@@ -20,11 +49,15 @@ const Login: React.FC<LoginProps> = ({ setToken }) => {
             <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input 
+                        type="text"
+                        onChange={e => setUserName(e.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input 
+                        type="password"
+                        onChange={e => setPassword(e.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
